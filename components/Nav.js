d9e5fcfe1babd2023e2c851f63cfed7627a804dd
Chomphoo <chomphoo@protonmail.com>
2022-07-27 19:41:07 +0200

    update nav to responsive
    + nav.scss
    - nav.module.scss
    + components MenuToggle
    + components use-dimensions
    
    /!\ Logo disapear when nav open is true THEN resize width > 1000px
    => Not essential
    
    Signed-off-by: Chomphoo <chomphoo@protonmail.com>

--- components/Nav.js
+++ components/Nav.js
@@ -1,35 +1,49 @@
 import Link from 'next/link';
 import logo from '../assets/css/logo.module.scss';
-import nav from '../assets/css/nav.module.scss';
-
+import * as React from "react";
+import { useRef } from "react";
+import { useDimensions } from "./use-dimensions";
+import { MenuToggle } from "../components/MenuToggle";
+import { motion, useCycle } from "framer-motion";
 
 
 function Nav() {
+  const [isOpen, toggleOpen] = useCycle(false, true);
+  const containerRef = useRef(null);
+  const { height } = useDimensions(containerRef);
 
   return (
+    <nav data-ismenu={true} data-isopen={isOpen}>
+      <motion.div
+        initial={false}
+        animate={isOpen ? "open" : "closed"}
+        custom={height}
+        ref={containerRef}
+      >
+        <MenuToggle toggle={() => toggleOpen()} />
+      </motion.div>
 
 
-    <nav className={nav.container} data-ismenu={true}>
 
-
-      <ul>
+      <ul className='navLeft'>
         <li ><Link href="/veille" passHref={true}><a>veille</a></Link></li>
         <li ><Link href="/how-to" passHref={true}><a>how to</a></Link></li>
       </ul>
 
-      <Link href="/" passHref={true}>
-        <a className={logo.container}>
-          <div>C</div>
-          <div>LB</div>
-        </a>
-      </Link>
+      {!isOpen && (<>
+        <Link href="/" passHref={true}>
+          <a className={logo.container}>
+            <div>C</div>
+            <div>LB</div>
+          </a>
+        </Link>
+      </>
+      )}
 
       <ul>
         <li><Link href="/" passHref={true}><a>me</a></Link></li>
-        <li><Link href="/contact" passHref={true}><a>{"contact >"}</a></Link></li>
+        <li><Link href="/contact" passHref={true}><a>contact</a></Link></li>
       </ul>
-
-
     </nav>
   )
 
--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import '../assets/css/chomphoo.scss'
 import '../assets/css/setFont.scss'
 import '../assets/css/framer.scss'
 import '../assets/css/grid.scss'
+import '../assets/css/nav.scss'
 
 
 function MyApp({ Component, pageProps }) {
